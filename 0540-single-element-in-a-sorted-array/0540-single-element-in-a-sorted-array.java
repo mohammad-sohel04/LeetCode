0a6d2ceb8222d542717648965a9b78481f928033
MOHAMMAD SOHEL <119403829+mohammad-sohel04@users.noreply.github.com>
2024-06-02 14:11:55 +0530

    Time: 0 ms (100%), Space: 50.2 MB (55.22%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -1,15 +1,24 @@
 class Solution {
-    public int singleNonDuplicate(int[] nums) {
-        if(nums.length==1)return nums[0];
-        if(nums[0]!=nums[1])return nums[0];
-        int n=nums.length;
-        if(nums[n-1]!=nums[n-2]){
-          return nums[n-1];
+    public int singleNonDuplicate(int[] arr) {
+        if(arr.length==1)return arr[0];
+        if(arr[0]!=arr[1])return arr[0];
+        int n=arr.length;
+        if(arr[n-1]!=arr[n-2]){
+          return arr[n-1];
         }
-        int ans=0;
-        for(int x:nums){
-          ans^=x;
+        int low=1,high=n-2;
+        while(low<=high){
+          int mid=(high+low)/2;
+          if(arr[mid]!=arr[mid-1] && arr[mid]!=arr[mid+1]){
+            return arr[mid];
+          }
+          if((mid%2==0 && arr[mid]==arr[mid-1]) || (mid%2!=0 && arr[mid]==arr[mid+1])){
+            high=mid;
+          }
+          else{
+            low=mid;
+          }
         }
-        return ans;
+        return -1;
     }
 }
