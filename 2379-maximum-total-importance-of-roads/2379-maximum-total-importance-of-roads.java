bf273cf91de486d2e45b740aa25d6ed72679e5ea
MOHAMMAD SOHEL <119403829+mohammad-sohel04@users.noreply.github.com>
2024-06-29 00:28:44 +0530

    Time: 113 ms (5.95%), Space: 68.8 MB (22.16%) - LeetHub

--- 2379-maximum-total-importance-of-roads/2379-maximum-total-importance-of-roads.java
+++ 2379-maximum-total-importance-of-roads/2379-maximum-total-importance-of-roads.java
@@ -7,26 +7,49 @@ class Solution {
             map.put(x,map.getOrDefault(x,0)+1);
           }
         }
-       ArrayList<ArrayList<Integer>>list=new ArrayList<>();
-       for(Map.Entry<Integer,Integer>entry:map.entrySet()){
-         int key=entry.getKey();
-         int val=entry.getValue();
-         ArrayList<Integer>temp=new ArrayList<>();
-         temp.add(key);
-         temp.add(val);
-         list.add(temp);
+        PriorityQueue<Map.Entry<Integer,Integer>>pq=new PriorityQueue<>(
+          new Comparator<Map.Entry<Integer,Integer>>(){
+            @Override
+            public int compare(Map.Entry<Integer,Integer>e1,Map.Entry<Integer,Integer>e2){
+              return e2.getValue()-e1.getValue();
+            }
+          }
+        );
+        int num=n;
+        for(Map.Entry<Integer,Integer>entry:map.entrySet()){
+          pq.add(entry);
+        }
+        long ans=0;
+        System.out.print(pq);
+
+      //  ArrayList<ArrayList<Integer>>list=new ArrayList<>();
+      //  for(Map.Entry<Integer,Integer>entry:map.entrySet()){
+      //    int key=entry.getKey();
+      //    int val=entry.getValue();
+      //    ArrayList<Integer>temp=new ArrayList<>();
+      //    temp.add(key);
+      //    temp.add(val);
+      //    list.add(temp);
+      //  }
+      //  int num=n;
+      //  Collections.sort(list,(a,b)->(b.get(1)-a.get(1)));
+      //  for(int i=0;i<list.size();i++){
+      //   map.put(list.get(i).get(0),num--);
+      //  }
+      //  long ans=0;
+      //  for(int i=0;i<roads.length;i++){
+      //   ans+=map.get(roads[i][0]);
+      //   ans+=map.get(roads[i][1]);
+      //  }
+       // System.out.print(map);
+       while(pq.isEmpty()==false){
+        Map.Entry<Integer,Integer>entry=pq.poll();
+        map.put(entry.getKey(),num--);
        }
-       int num=n;
-       Collections.sort(list,(a,b)->(b.get(1)-a.get(1)));
-       for(int i=0;i<list.size();i++){
-        map.put(list.get(i).get(0),num--);
-       }
-       long ans=0;
        for(int i=0;i<roads.length;i++){
         ans+=map.get(roads[i][0]);
         ans+=map.get(roads[i][1]);
        }
-       // System.out.print(map);
         return ans;
     }
 }
