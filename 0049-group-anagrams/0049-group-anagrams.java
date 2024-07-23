848caaa1f9a2183808e3ee9c95482636cbbb2c92
MOHAMMAD SOHEL <119403829+mohammad-sohel04@users.noreply.github.com>
2024-07-23 23:36:54 +0530

    Time: 7 ms (68.94%), Space: 47.8 MB (47.7%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,36 +1,21 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        List<List<String>>list=new ArrayList<>();
-        int n=strs.length;
-        for(int i=0;i<n;i++){
-          if(strs[i]=="-1")continue;
-          List<String>temp=new ArrayList<>();
-          temp.add(strs[i]);
-          for(int j=i+1;j<n;j++){
-            if(helper(strs[i],strs[j])){
-              temp.add(strs[j]);
-              strs[j]="-1";
-            }
+        Map<String,List<String>>map=new HashMap<>();
+        for(String s:strs){
+          char arr[]=s.toCharArray();
+          Arrays.sort(arr);
+          String t=new String(arr);
+          if(map.containsKey(t)){
+            List<String>temp=map.get(t);
+            temp.add(s);
+            map.put(t,temp);
           }
-          list.add(temp);
+          else{
+            List<String>temp=new ArrayList<>();
+            temp.add(s);
+            map.put(t,temp);
+          }
         }
-        return list;
-    }
-    boolean helper(String s1,String s2){
-      if(s1.length()!=s2.length())return false;
-      int arr[]=new int[26];
-      for(char ch:s1.toCharArray()){
-        if(ch>='a' && ch<='z')arr[ch-'a']++;
-    }
-    //int pre=set.size();
-    for(char ch:s2.toCharArray()){
-      //set.add(ch);
-      if(ch>='a' && ch<='z')arr[ch-'a']--;
-    }
-    for(int x:arr){
-      if(x!=0)return false;
-    }
-    return true;
-
+        return new ArrayList<>(map.values());
     }
 }
