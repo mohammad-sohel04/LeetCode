17419a73b3d99c4997e7826fd5ed47fd1e977128
MOHAMMAD SOHEL <119403829+mohammad-sohel04@users.noreply.github.com>
2024-07-27 11:48:55 +0530

    Time: 14 ms (86.36%), Space: 48.2 MB (30.03%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.java
+++ 0044-wildcard-matching/0044-wildcard-matching.java
@@ -1,29 +1,29 @@
 class Solution {
-  int  dp[][];
+  Boolean dp[][];
     public boolean isMatch(String s, String p) {
-       dp=new int[s.length()+1][p.length()+1];
-       for(int m[]:dp){
-        Arrays.fill(m,-1);
-       }
-       return helper(s,p,s.length()-1,p.length()-1)==0?false:true;
+       dp=new Boolean[s.length()+1][p.length()+1];
+      //  for(boolean m[]:dp){
+      //   Arrays.fill(m,false);
+      //  }
+       return helper(s,p,s.length()-1,p.length()-1);
     }
-    int helper(String s,String p,int i,int j){
-      if(i<0 && j<0)return 1;
-      if(j<0 && i>=0)return 0;
+    boolean helper(String s,String p,int i,int j){
+      if(i<0 && j<0)return true;
+      if(j<0 && i>=0)return false;
       if(i<0 && j>=0){
         for(int k=0;k<=j;k++){
-          if(p.charAt(k)!='*')return 0;
+          if(p.charAt(k)!='*')return false;
         }
-        return 1;
+        return true;
       }
-      if(dp[i][j]!=-1)return dp[i][j];
+      if(dp[i][j]!=null)return dp[i][j];
       if(s.charAt(i)==p.charAt(j) ||  p.charAt(j)=='?'){
         return dp[i][j]=helper(s,p,i-1,j-1);
       }
       if(p.charAt(j)=='*'){
-        return dp[i][j]=helper(s,p,i,j-1) | helper(s,p,i-1,j);
+        return dp[i][j]=helper(s,p,i,j-1) || helper(s,p,i-1,j);
       }
-      return dp[i][j]=0;
+      return dp[i][j]=false;
 
     }
 }
